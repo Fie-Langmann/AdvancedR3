1f38e44ffa0534bf3b38c9e89007c8e5b3f368c6
Fie Langmann <fielangmann@gmail.com>
2024-12-10 16:16:50 +0100

    minor edits

--- R/functions.R
+++ R/functions.R
@@ -7,6 +7,6 @@
 descriptive_stats <- function(data) {
     lipidomics |>
         dplyr::group_by(metabolite) |>
-        dplyr::summarise(across(value, list(mean = mean, sd = sd))) |>
-        dplyr::mutate(across(where(is.numeric), ~ round(.x, digits = 1)))
+        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+        dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = 1)))
 }
