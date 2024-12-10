5949c266d480918263009e0a059c85b71117ad64
Fie Langmann <fielangmann@gmail.com>
2024-12-10 16:06:10 +0100

    added descriptive_stats function

---
+++ R/functions.R
@@ -0,0 +1,12 @@
+#' descriptive stats function
+#'
+#' @param data
+#'
+#' @return A data.frame/tibble
+
+descriptive_stats <- function(data) {
+    lipidomics |>
+        dplyr::group_by(metabolite) |>
+        dplyr::summarise(across(value, list(mean = mean, sd = sd))) |>
+        dplyr::mutate(across(where(is.numeric), ~ round(.x, digits = 1)))
+}
