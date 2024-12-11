53dd77f65b2d3f09970122091a2915332fddfa6b
Fie Langmann <fielangmann@gmail.com>
2024-12-11 09:46:00 +0100

    minor edits

--- R/functions.R
+++ R/functions.R
@@ -5,7 +5,7 @@
 #' @return A data.frame/tibble
 
 descriptive_stats <- function(data) {
-    lipidomics |>
+    data |>
         dplyr::group_by(metabolite) |>
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
         dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = 1)))
