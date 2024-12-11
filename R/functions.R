e954929f786572b7f8bddba9a77d49d7a8b1aa0c
Fie Langmann <fielangmann@gmail.com>
2024-12-11 14:17:09 +0100

    minor edits

--- R/functions.R
+++ R/functions.R
@@ -3,7 +3,7 @@
 #' @param data
 #'
 #' @return A data.frame/tibble
-
+#'
 descriptive_stats <- function(data) {
   data |>
     dplyr::group_by(metabolite) |>
@@ -17,10 +17,40 @@ descriptive_stats <- function(data) {
 #' @param data
 #'
 #' @return Histograms for metabolites
-
+#'
 plot_distributions <- function(data) {
   ggplot2::ggplot(data, ggplot2::aes(x = value)) +
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
 }
 
+
+
+#' Convert a column's character values to snakecase format.
+#'
+#' @param data The lipidomics dataset.
+#' @param columns The column you want to convert into the snakecase format.
+#'
+#' @return A data frame.
+#'
+column_values_to_snake_case <- function(data, columns) {
+    data |>
+        dplyr::mutate(dplyr::across({{ columns }}, snakecase::to_snake_case))
+}
+
+
+#' Convert data from long to wide format.
+#'
+#' @param data
+#'
+#' @return A wide data frame.
+#'
+metabolites_to_wider <- function(data){
+    data |>
+        tidyr::pivot_wider(
+            names_from = metabolite,
+            values_from = value,
+            values_fn = mean,
+            names_prefix = "metabolite_"
+        )
+}
