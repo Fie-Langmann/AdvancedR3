dc55d0c740d39ecbc07a3e01e0a7aabff860efeb
Fie Langmann <fielangmann@gmail.com>
2024-12-11 10:22:09 +0100

    minor edits

--- R/functions.R
+++ R/functions.R
@@ -5,8 +5,22 @@
 #' @return A data.frame/tibble
 
 descriptive_stats <- function(data) {
-    data |>
-        dplyr::group_by(metabolite) |>
-        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
-        dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = 1)))
+  data |>
+    dplyr::group_by(metabolite) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+    dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+
+#' histogram function
+#'
+#' @param data
+#'
+#' @return Histograms for metabolites
+
+plot_distributions <- function(data) {
+  ggplot2::ggplot(data, ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
+
